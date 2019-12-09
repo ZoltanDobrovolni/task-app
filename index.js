44344a49ed826c4abd436c8aafd9942d768827ea
zoltan.dobrovolni <zoltan.dobrovolni@automizy.com>
2019-12-09 08:23:09 +0100

    Fix CORS issue

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 require('./src/db/mongoose.db');
 const userRouter = require('./src/routes/userRoutes');
 const taskRouter = require('./src/routes/taskRoutes');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3002;
+const origin = process.env.ORIGIN || 'http://localhost:3000'
+
+
+app.use(cors({
+    origin,
+    credentials: false
+}));
 
 app.use(express.json());
 app.use(userRouter);
